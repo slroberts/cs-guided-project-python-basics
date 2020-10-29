295f84afe34ed0277d6e2cfe5e4cbbd1c422aec5
Shomari Roberts <shomariroberts@gmail.com>
2020-10-28 20:32:48 -0400

    completed

--- src/demonstration_07.py
+++ src/demonstration_07.py
@@ -12,5 +12,25 @@ Examples:
 - nth_smallest([7, 3, 5, 1], 2) ➞ 3
 """
 def nth_smallest(lst, n):
-    # Your code here
+    """
+    a function that returns the nth smallest element
+    (the smallest element is the first smallest, the second smallest element is the
+    second smallest, etc).
+    """
+
+    # first sort list in order
+    lst.sort()
+
+    # if n is greater than the length of the lst
+    if n > len(lst):
+        # return "None"
+        return "None"
+
+    # return the smallest number from the lst on the index to the caller
+    return lst[n - 1]
+
+print(nth_smallest([7, 5, 3, 1], 1))
+print(nth_smallest([1, 3, 5, 7], 3))
+print(nth_smallest([1, 3, 5, 7], 5))
+print(nth_smallest([7, 3, 5, 1], 2))
 
--- src/demonstration_08.py
+++ src/demonstration_08.py
@@ -10,6 +10,15 @@ Examples:
 - num_args(True, False) ➞ 2
 - num_args({}) ➞ 1
 """
-def num_args():
-    # Your code here
+def num_args(*args):
+    """
+    a function that returns the number of arguments it was called with.
+    """
+    # return the number of arguments in the function to the caller
+    return( len(args))
 
+print(num_args())
+print(num_args("foo"))
+print(num_args("foo", "bar"))
+print(num_args(True, False))
+print(num_args({}))
